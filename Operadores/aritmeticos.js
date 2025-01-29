807737bcee2af929adf7898e188c7347537411cf
mariaaaa08fmb <maria.c.barros17@aluno.senai.br>
2025-01-29 08:19:11 -0300

    Initial commit

---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,24 @@
+console.log("Olá, Mundo!")
+
+let nome = "Juliana"
+var nomes = "Juliana"
+
+console.log(nome)
+
+let idade = 24
+console.log("minha idade é", idade, "anos")
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let multi = a * b
+let div = a/ b
+let resto = a % b
+
+console.log (soma)
+console.log (sub)
+console.log (multi)
+console.log (div)
+console.log (resto)
---
+++ lista1/lista.js
@@ -0,0 +1,48 @@
+//ex 1
+let a = 7
+let b = 2
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+//ex 2
+
+let altura = 19
+let largura = 28
+
+let multi = largura * altura
+
+console.log(mult)
+
+// ex 3
+
+let num = 9
+
+let multti = 9 * 3
+let divi = 9 / 3
+
+console.log(mult)
+console.log(divi)
+
+// ex 4
+
+let nota1 = 6
+let nota2 = 4
+let nota3 = 9
+
+let somaa = nota1 + nota2 + nota3
+let divisao = soma / 5
+
+console.log(somaa)
+console.log(divisao)
+
+// ex 5
