f6dff6c7b7722e05d499f395aa3413a9d5b732b1
mariaaaa08fmb <maria.c.barros17@aluno.senai.br>
2025-02-25 08:51:12 -0300

    ptt

--- estrutura condicional/PPT/ppt.js
+++ estrutura condicional/PPT/ppt.js
@@ -1,27 +1,44 @@
 // Jogo Pedra, Papel e Tesoura
+let placarJogador = 0
+let placarPc = 0
 
-const escolhaJogador = prompt("EScolha pedra, papel ou tesoura: ")
-// solicita que o usuário informe qual ele quer
+let rodadas = Number(prompt("Quantas vezes você deseja jogar?"))
 
-const escolhaPC = [ "Pedra", "Papel", "Tesoura"]
-[ Math.floor(Math.random ()* 3 )]
 
-//Gera uma escolha aleatoria para o computador
-// Criamos um array com as opções e usamos o radom para escolher uma de forma aleatoria
+for (let i = 0; i < rodadas; i++) {
 
-alert('Você escolheu $ {EscolhaJogador}')
-alert('O computador escolheu ${EscolhaPC}')
+    const EscolhaJogador = prompt("EScolha pedra, papel ou tesoura: ")
+    // solicita que o usuário informe qual ele quer
 
-// mostrando qual foi a nossa escolha e qual foi a escolha do computador
+    const EscolhaPC = ["pedra", "papel", "tesoura"][Math.floor(Math.random() * 3)]
 
-//Compara se a nossa escolha foi igual a do computador
+    //Gera uma escolha aleatoria para o computador
+    // Criamos um array com as opções e usamos o radom para escolher uma de forma aleatoria
 
-if (EscolhaJpgador == EscolhaPC) {
-    alert("Empate")
-} else if (EscolhaJogador == "pedra" && EscolhaPC == "papel") {
-    alert ("Você venceu")
-} else if (EscolhaJogador == "papel" && EscolhaPC == "pedra") {
-    alert ("Você venceu")    
-} else {
-    alert ("Você perdeu")
-}
+    alert(`Você escolheu ${EscolhaJogador}`)
+    alert(`O computador escolheu ${EscolhaPC}`)
+
+    // mostrando qual foi a nossa escolha e qual foi a escolha do computador
+
+    //Compara se a nossa escolha foi igual a do computador
+
+    if (EscolhaJogador == EscolhaPC) {
+        alert("Empate")
+    } else if (EscolhaJogador == "pedra" && EscolhaPC == "papel") {
+        alert("Você venceu")
+        placarJogador++
+    } else if (EscolhaJogador == "tesoura" && EscolhaPC == "papel") {
+        alert("Você venceu")
+        placarJogador++
+    } else if (EscolhaJogador == "papel" && EscolhaPC == "pedra") {
+        alert("Você venceu")
+        placarJogador++
+    } else {
+        alert("Você perdeu")
+    }
+    console.log("Placar Jogador; ", placarJogador)
+    console.log("Placar Computador; ", placarPc)
+
+}     
+
+alert(`Fim de jogo! Placar final: Jogador${placarJogador}x ${placarPc} Computador`)
